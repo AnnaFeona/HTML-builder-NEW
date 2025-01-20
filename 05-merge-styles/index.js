c9764a922322d39ef85ab9ce80a9266cf15af6c8
Hanna Hlavatskikhe <glavatskikh.anna@gmail.com>
2025-01-21 00:33:20 +0300

    feat: 05-merge-styles implemented

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { stdout } = process;
+
+const folder = path.join(__dirname, 'styles');
+const newFolder = path.join(__dirname, 'project-dist');
+
+mergeCSS(folder, newFolder, 'bundle.css');
+
+async function mergeCSS(folder, newFolder, fileName) {
+  try {
+    const files = await fs.readdir(folder, { withFileTypes: true });
+    const cssFiles = files.filter(
+      (item) => path.extname(item.name) === '.css' && !item.isDirectory(),
+    );
+
+    const content = await Promise.all(
+      cssFiles.map((item) => fs.readFile(path.join(folder, item.name))),
+    );
+
+    await fs.writeFile(path.join(newFolder, fileName), content.join('\n'));
+
+    stdout.write('File bundle.css has been successfully created.');
+  } catch (err) {
+    throw new Error(`Error merging CSS: ${err.message}`);
+  }
+}
+
+exports.mergeCSS = mergeCSS;
